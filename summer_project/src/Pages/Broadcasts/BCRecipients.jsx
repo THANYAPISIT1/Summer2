5fc2e0b102e12552537fe5967545ed47e16eea2f
THANYAPISIT1 <65025895@up.ac.th>
2024-04-20 21:23:24 +0700

    Update before deploy

--- summer_project/src/Pages/Broadcasts/BCRecipients.jsx
+++ summer_project/src/Pages/Broadcasts/BCRecipients.jsx
@@ -1,7 +1,15 @@
+import TopNav from "../../Components/Layouts/TopNav";
+import Sidebar from "../../Components/Layouts/Sidebar";
+
 const BCRecipients = () => {
     return (
         <div>
-            
+            <TopNav/>
+            <Sidebar />
+            <div className="ml-64 mt-16">
+
+            </div>
+
         </div>
     );  
 };
--- summer_project/src/Pages/Customers/Customer.jsx
+++ summer_project/src/Pages/Customers/Customer.jsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { IoPersonOutline } from "react-icons/io5";
-
+import TopNav from '../../Components/Layouts/TopNav';
+import Sidebar from '../../Components/Layouts/Sidebar';
 
 const Customer = () => {
 
@@ -26,28 +27,32 @@ const Customer = () => {
 
 
   return (
-    <div className='ml-64'>
-         
-        <header className="flex justify-between items-center bg-white text-black text-3xl font-bold pl-8 px-2 py-4 mb-8 border-b border-gray-300">
-          <h2>Customers</h2>
-          <Link to="/AddNewCustomer" ><button className='bg-blue-600 text-white text-xl no-underline py-2 px-4 rounded-custom cursor-pointer'>Add new customer</button></Link>
-          
-        </header>
-       <div className=' border-b rounded-xl border-gray-700 flex justify-between items-center  px-4 py-2 ml-6 mr-16'>
-        <div className='flex '>
-          <div>
-            <IoPersonOutline className='text-6xl'/>
-          </div>
-          <div className='pl-5'>
-            <div className='flex '>
-              <p className='font-medium font-sans text-xl rounded pl-4 pr-2 content-center'>{name}</p>
-             <div className={`content-center text-xs border rounded-xl px-2 ${levelColors[level] || 'bg-gray-400'}`}>{level}</div>
+    <div>
+        <TopNav />
+        <Sidebar />
+      <div className='ml-64 mt-16'>
+
+          <header className="flex justify-between items-center bg-white text-black text-3xl font-bold pl-8 px-2 py-4 mb-8 border-b border-gray-300">
+            <h2>Customers</h2>
+            <Link to="/AddNewCustomer" ><button className='bg-blue-600 text-white text-xl no-underline py-2 px-4 rounded-custom cursor-pointer'>Add new customer</button></Link>
+            
+          </header>
+        <div className=' border-b rounded-xl border-gray-700 flex justify-between items-center  px-4 py-2 ml-6 mr-16'>
+          <div className='flex '>
+            <div>
+              <IoPersonOutline className='text-6xl'/>
             </div>
-            <div className='flex font-medium font-sans text-sm rounded py-0.5 px-4 mr-1/2 pb-2.5 '><h4>Email:</h4><p className='font-medium font-sans text-sm rounded pl-1'>{email}</p></div></div>  
+            <div className='pl-5'>
+              <div className='flex '>
+                <p className='font-medium font-sans text-xl rounded pl-4 pr-2 content-center'>{name}</p>
+              <div className={`content-center text-xs border rounded-xl px-2 ${levelColors[level] || 'bg-gray-400'}`}>{level}</div>
+              </div>
+              <div className='flex font-medium font-sans text-sm rounded py-0.5 px-4 mr-1/2 pb-2.5 '><h4>Email:</h4><p className='font-medium font-sans text-sm rounded pl-1'>{email}</p></div></div>  
+          </div>
+          <Link to="/DetailAndEdit"><button className='bg-green-600 text-white text-xl no-underline py-2 px-4 rounded-custom cursor-pointer '>Detail</button></Link>
         </div>
-        <Link to="/DetailAndEdit"><button className='bg-green-600 text-white text-xl no-underline py-2 px-4 rounded-custom cursor-pointer '>Detail</button></Link>
-       </div>
-    
+      
+      </div>
     </div>
   )
 }
