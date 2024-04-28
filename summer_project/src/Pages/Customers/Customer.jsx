2b503fb149e90c77313e296b54ec6079cb43fb20
Tnkwp <119997936+Tnkwp@users.noreply.github.com>
2024-04-28 23:32:19 +0700

    new

--- summer_project/src/Pages/Customers/Customer.jsx
+++ summer_project/src/Pages/Customers/Customer.jsx
@@ -30,11 +30,11 @@ const Customer = () => {
     <div>
         <TopNav />
         <Sidebar />
-      <div className='ml-64 mt-16'>
+      <div className='ml-64 mt-16 py-3'>
 
-          <header className="flex justify-between items-center bg-white text-black text-3xl font-bold pl-8 px-2 py-4 mb-8 border-b border-gray-300">
+          <header className="flex justify-between items-center bg-white text-black text-3xl font-bold pl-8 px-2 py-4 mb-8 border-b border-gray-200 ">
             <h2>Customers</h2>
-            <Link to="/AddNewCustomer" ><button className='bg-blue-600 text-white text-xl no-underline py-2 px-4 rounded-custom cursor-pointer'>Add new customer</button></Link>
+            <Link to="/customer/add" ><button className='bg-blue-600 text-white text-xl no-underline py-2 px-4 rounded-custom cursor-pointer'>Add new customer</button></Link>
             
           </header>
         <div className=' border-b rounded-xl border-gray-700 flex justify-between items-center  px-4 py-2 ml-6 mr-16'>
@@ -49,7 +49,7 @@ const Customer = () => {
               </div>
               <div className='flex font-medium font-sans text-sm rounded py-0.5 px-4 mr-1/2 pb-2.5 '><h4>Email:</h4><p className='font-medium font-sans text-sm rounded pl-1'>{email}</p></div></div>  
           </div>
-          <Link to="/DetailAndEdit"><button className='bg-green-600 text-white text-xl no-underline py-2 px-4 rounded-custom cursor-pointer '>Detail</button></Link>
+          <Link to="/customer/detail"><button className='bg-green-600 text-white text-xl no-underline py-2 px-4 rounded-custom cursor-pointer '>Detail</button></Link>
         </div>
       
       </div>
