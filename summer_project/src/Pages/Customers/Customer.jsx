7c98f1254e54ebb4f2775d0bceb596d49c9cc6b5
Tnkwp <119997936+Tnkwp@users.noreply.github.com>
2024-05-03 23:09:27 +0700

    updata customer

--- summer_project/src/Pages/Customers/Customer.jsx
+++ summer_project/src/Pages/Customers/Customer.jsx
@@ -37,7 +37,7 @@ const Customer = () => {
             <Link to="/customer/add" ><button type="button" className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Add new customer</button></Link>
             
           </header>
-        <div className=' border-b rounded-xl border-gray-700 flex justify-between items-center  px-4 py-2 ml-6 mr-16 '>
+        <div className=' shadow-xl hover:bg-opacity-75 rounded-xl  flex justify-between items-center  px-4 py-2 ml-6 mr-16 '>
           <div className='flex '>
             <div>
               <IoPersonOutline className='text-6xl'/>
